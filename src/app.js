94becca592db5487cbfd0fa5de8bfdf91e4b1e57
AlejandroGuilarte <128685565+guilarte100192@users.noreply.github.com>
2025-05-23 10:22:29 +0000

    Cartas Random

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
+let carta = ["♥", "♦", "♣", "♠"]
+let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function cardRandom() {
+  let muestraCarta = carta [Random(carta.length)];
+  let muestraNumero = numero [Random(numero.length)];
+  let lacarta = `${muestraCarta}`;
+  let elnumero = `${muestraNumero}`;
+  document.querySelector("#signo").innerHTML = lacarta;
+  document.querySelector("#valor").innerHTML = elnumero;
+  document.querySelector("#signo2").innerHTML = lacarta;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function Random (nun) {
+  return Math.floor(Math.random() * nun)
+}
+
+const boton = document.querySelector("#btn")
+boton.addEventListener("click", cardRandom)
+
+window.onload = cardRandom
