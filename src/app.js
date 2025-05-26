b547b618830f49c28d61853c07993c2d8a9638da
AlejandroGuilarte <128685565+guilarte100192@users.noreply.github.com>
2025-05-26 08:05:33 +0000

    RandomCard

--- src/app.js
+++ src/app.js
@@ -7,11 +7,19 @@ let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 function cardRandom() {
   let muestraCarta = carta [Random(carta.length)];
   let muestraNumero = numero [Random(numero.length)];
+      const elemento = document.querySelector("#signo")
+      const elemento2 = document.querySelector("#signo2")
   let lacarta = `${muestraCarta}`;
   let elnumero = `${muestraNumero}`;
   document.querySelector("#signo").innerHTML = lacarta;
   document.querySelector("#valor").innerHTML = elnumero;
   document.querySelector("#signo2").innerHTML = lacarta;
+
+  if (muestraCarta === "♥" || muestraCarta === "♦"){
+    [elemento, elemento2].forEach(el => el.style.color = "red")
+  } else {
+    [elemento, elemento2].forEach(el => el.style.color = "black")
+  }
 }
 
 function Random (nun) {
